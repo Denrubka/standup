21723d0ea510deeca123f3b81377ff4a7b4b9ad3
Denrubkau <denrubka2@mail.ru>
2023-12-06 14:39:24 +0700

    finish

--- index.js
+++ index.js
@@ -1,45 +1,83 @@
-import http from 'node:http';
+import http from "node:http";
 import fs from "node:fs/promises";
+import { sendData, sendError } from "./modules/send.js";
+import { checkFile } from "./modules/checkFile.js";
+import { handleComediansRequest } from "./modules/handleComediansRequest.js";
+import { handleAddClient } from "./modules/handleAddClient.js";
+import { handleClientsRequest } from "./modules/handleClientsRequest.js";
+import { handleUpdateClient } from "./modules/handleUpdateClient.js";
 
 const PORT = 8080;
+const COMEDIANS = "./comedians.json";
+export const CLIENTS = "./clients.json";
 
-http
-    .createServer(async (req, res) => {
+const startServer = async () => {
+    if (!(await checkFile(COMEDIANS))) {
+        return;
+    }
 
-        await fs.writeFile('clients.json', '[]');
+    await checkFile(CLIENTS, true);
+    const comediansData = await fs.readFile(COMEDIANS, "utf-8");
+    const comedians = JSON.parse(comediansData);
 
+    http.createServer(async (req, res) => {
         try {
-            await fs.access('comedians.json');
-            if(req.method === "GET" && req.url === "/comedians") {
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader(
+                "Access-Control-Allow-Methods",
+                "GET, POST, PATCH, OPTIONS"
+            );
+            res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-                try {
-                    const data = await fs.readFile('comedians.json', 'utf-8');
-                    res.writeHead(200, {
-                        "Content-Type": "text/json; charset=UTF-8",
-                        "Access-Control-Allow-Origin": "*",
-                    });
-                    res.end(data);
-                } catch(error) {
-                    res.writeHead(500, {
-                        "Content-Type": "text/plain; charset=UTF-8",
-                    });
-                    res.end(`Ошибка сервера ${error}`);
-                }
-
-            } else {
-                res.writeHead(404, {
-                    "Content-Type": "text/plain; charset=UTF-8",
-                });
-                res.end('Page not found');
+            if (req.method === "OPTIONS") {
+                res.writeHead(204);
+                res.end();
+                return;
             }
-        } catch {
-            res.writeHead(404, {
-                "Content-Type": "text/plain; charset=UTF-8",
-            });
-            res.end('File not found');
+
+            const segments = req.url.split("/").filter(Boolean);
+
+            if (req.method === "GET" && segments[0] === "comedians") {
+                handleComediansRequest(req, res, comedians, segments);
+                return;
+            }
+
+            if (req.method === "POST" && segments[0] === "clients") {
+                //POST /clients
+                //Добавление клиента
+                handleAddClient(req, res);
+                return;
+            }
+
+            if (
+                req.method === "GET" &&
+                segments[0] === "clients" &&
+                segments.length === 2
+            ) {
+                //GET /clients:ticket
+                //Получение клиента по номеру билета
+                const ticketNumber = segments[1];
+                handleClientsRequest(req, res, ticketNumber);
+                return;
+            }
+
+            if (
+                req.method === "PATCH" &&
+                segments[0] === "clients" &&
+                segments.length === 2
+            ) {
+                //PATCH /clients:ticket
+                //Обновление клиента
+                handleUpdateClient(req, res, segments);
+                return;
+            }
+            sendError(res, 404, "Page not found");
+        } catch (error) {
+            sendError(res, 500, `Ошибка сервера ${error}`);
         }
-    })
-    .listen(PORT);
+    }).listen(PORT);
 
-console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
+    console.log(`Сервер запущен по адресу http://localhost:${PORT}`);
+};
 
+startServer();
